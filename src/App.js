aef31bdb4c451c90f88fd77a2b01cca828369c03
Utkarsh Dubey <utkarshdubey44@gmail.com>
2023-10-28 00:45:12 +0530

    Improving dark mode and refactor the code.

--- src/App.js
+++ src/App.js
@@ -2,19 +2,37 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-
+import React, { useState } from 'react'
 
 function App() {
+  const [mode, setMode] = useState('light') //whether dark mode is enabled or not
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = 'grey'
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = "white";
+    }
+
+  }
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About" />
+      <Navbar
+        title="TextUtils"
+        aboutText="About"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
       <div className="row">
         <div className="col-2">
           <img src={logo} className="App-logo" alt="logo" />
         </div>
       </div>
       <div className="container my-3">
-        <TextForm heading="Enter the text to analyse" />
+        <TextForm mode={mode} heading="Enter the text to analyse"/>
       </div>
     </>
   );
