1d93cf0ce4213fb56cf527c1ca70d562301a9460
Utkarsh Dubey <utkarshdubey44@gmail.com>
2023-10-31 04:48:35 +0530

    a overview for React routes uses

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 import React, { useState } from 'react'
 import Alert from './components/Alert';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light'); //whether dark mode is enabled or not
@@ -49,29 +51,43 @@ function App() {
 
   return (
     <>
-      <Navbar
-        title="TextUtils"
-        aboutText="About"
-        mode={mode}
-        toggleMode={toggleMode}
-        toggleModeB={toggleModeB}
-      />
-      {/* <div className="row">
+      <Router>
+        <Navbar
+          title="TextUtils"
+          aboutText="About"
+          mode={mode}
+          toggleMode={toggleMode}
+          toggleModeB={toggleModeB}
+        />
+        {/* <div className="row">
         <div className="col-2">
           <img src={logo} className="App-logo" alt="logo" />
         </div>
       </div> */}
-      <div className="d-flex">
-        <div className="p-2">
-          <img src={logo} className="App-logo" alt="logo" />
+        <div className="d-flex">
+          <div className="p-2">
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+          <div className="p-2 flex-grow-1 m-3">
+            <Alert alert={alert} />
+          </div>
         </div>
-        <div className="p-2 flex-grow-1 m-3">
-          <Alert alert={alert} />
+        <div className="container my-3">
+          <Switch>
+          {/* As react to partial matching so for this we use exact to match same keyword of routes */}
+            <Route exact path="/about">
+              <About />
+            </Route>
+            <Route exact path="/">
+              <TextForm
+                mode={mode}
+                showAlert={showAlert}
+                heading="Enter the text to analyse"
+              />
+            </Route>
+          </Switch>
         </div>
-      </div>
-      <div className="container my-3">
-        <TextForm mode={mode} showAlert={showAlert} heading="Enter the text to analyse" />
-      </div>
+      </Router>
     </>
   );
 }
