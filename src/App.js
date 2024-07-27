033f5815a005fad12a928db8ce1c05cabc8210e5
U <utkarshdubey44@gmail.com>
2024-07-27 23:39:11 +0530

    fixes and changes in the app

--- src/App.js
+++ src/App.js
@@ -1,51 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
-import React, { useState } from 'react'
-import Alert from './components/Alert';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import React, { useState } from "react";
+import Alert from "./components/Alert";
 
 function App() {
-  const [mode, setMode] = useState('light'); //by default light mode
+  const [mode, setMode] = useState("light"); //by default light mode
   const [alert, setAlert] = useState(null);
 
   // It's for showing alert
-  const showAlert = (message, type)=>{
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
-      type: type
-    })
+      type: type,
+    });
     setTimeout(() => {
       setAlert(null);
     }, 1650);
-  }
+  };
 
-  const toggleMode = ()=>{
-    if(mode === 'light'){
-      setMode('dark');
-      document.body.style.backgroundColor = 'grey';
-      showAlert("Dark mode has been enabled!","success")
-      document.title = "TextUtils - Dark Mode"
-    }
-    else{
-      setMode('light');
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "grey";
+      showAlert("Dark mode has been enabled!", "success");
+      document.title = "TextUtils - Dark Mode";
+    } else {
+      setMode("light");
       document.body.style.backgroundColor = "white";
       showAlert("Light mode has been enabled!", "success");
       document.title = "TextUtils";
     }
-  }
-  const toggleModeB = ()=>{
-    if(mode === 'light'){
-      setMode('primary');
+  };
+  const toggleModeB = () => {
+    if (mode === "light") {
+      setMode("primary");
       document.body.style.backgroundColor = "#0e324b";
-      showAlert("Blue mode has been enabled!","success")
-    }
-    else{
-      setMode('light');
+      showAlert("Blue mode has been enabled!", "success");
+    } else {
+      setMode("light");
       document.body.style.backgroundColor = "white";
       showAlert("Light mode has been enabled!", "success");
     }
-  }
+  };
 
   return (
     <>
@@ -70,7 +68,11 @@ function App() {
         </div>
       </div>
       <div className="container my-3">
-        <TextForm mode={mode} showAlert={showAlert} heading="Enter the text to analyse" />
+        <TextForm
+          mode={mode}
+          showAlert={showAlert}
+          heading="Enter the text to Analyse"
+        />
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,53 +1,116 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from "react";
+
+const calculateWordFrequency = (text) => {
+  const words = text.split(/\s+/).filter(Boolean);
+  const wordFrequency = words.reduce((acc, word) => {
+    // Remove non-alphanumeric characters only if they appear at the end of the word
+    word = word.replace(/[^a-zA-Z0-9]+$/, "");
+
+    if (word.length > 4) {
+      word = word.toLowerCase();
+      acc[word] = (acc[word] || 0) + 1;
+    }
+    return acc;
+  }, {});
+  return wordFrequency;
+};
 
 export default function TextForm(props) {
-    const handleUpClick = ()=> {
-        // console.log("Uppercase was clicked!!")
-        let newText = text.toUpperCase();
-        setText(newText);
-        props.showAlert("Text changed to Upper Case!", "success");
-    }
+  const [text, setText] = useState("Enter text here");
+  const [wordCount, setWordCount] = useState({});
+  const [showWordFrequency, setShowWordFrequency] = useState(false);
+  // let typingTimeout = null;
 
-    const handleDownClick = () => {
+  // useEffect(() => {
+  //   if (text.length > 0) {
+  //     clearTimeout(typingTimeout);
+  //     typingTimeout = setTimeout(() => {
+  //       analyzeText();
+  //     }, 2000);
+  //   }
+  //   return () => clearTimeout(typingTimeout);
+  // }, [text]);
+
+  const handleUpClick = () => {
+    // console.log("Uppercase was clicked!!")
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("Text changed to Upper Case!", "success");
+  };
+
+  const handleDownClick = () => {
     //   console.log("Lowercase was clicked!!");
-      let newText = text.toLocaleLowerCase();
-      setText(newText);
+    let newText = text.toLocaleLowerCase();
+    setText(newText);
     props.showAlert("Text changed to Lower Case!", "success");
+  };
 
-    }
+  const handleClearClick = () => {
+    let newText = "";
+    setText(newText);
+    props.showAlert("Text cleared!", "success");
+  };
 
-    const handleClearClick = () => {
-      let newText = '';
-      setText(newText);
-      props.showAlert("Text cleared!", "success");
+  const handleCopy = () => {
+    var text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+    props.showAlert("Text copied to Clipboard!", "success");
+  };
 
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[  ]+/);
+    if (newText.length !== 1) {
+      setText(newText.join(" ").trim());
+      props.showAlert("Extra Spaces removed from the Text!", "success");
+    } else {
+      props.showAlert("There are no spaces to remove!", "warning");
     }
+  };
 
-    
-    const handleCopy = ()=>{
-        var text = document.getElementById("myBox");
-        text.select();
-        navigator.clipboard.writeText(text.value);
-        props.showAlert("Text copied to Clipboard!", "success");
-    }
-    
-    const handleExtraSpaces = ()=>{
-        let newText = text.split(/[ ]+/);
-        if (newText.length !== 1){
-            setText(newText.join(" "))
-            props.showAlert("Extra Spaces removed from the Text!", "success");
-        }else{
-            props.showAlert("There are no spaces to remove!", "warning");
-        }
-    }
+  const handleOnChange = (event) => {
+    // console.log("Change was clicked!!")
+    // its going to chage the text value to the data entered in text area
+    setText(event.target.value);
+  };
 
-    const handleOnChange = (event)=> {
-        // console.log("Change was clicked!!")
-        // its going to chage the text value to the data entered in text area
-        setText(event.target.value)
-    }
-    
-    const [text, setText] = useState('Enter text here');
+  const analyzeText = () => {
+    const words = text.split(/\s+/).filter(Boolean);
+    const wordFrequency = words.reduce((acc, word) => {
+      // Remove any non-alphanumeric characters if they appear at the end
+      word = word.replace(/[^a-zA-Z0-9]+$/, "");
+      if (word.length > 4) {
+        word = word.toLowerCase();
+        acc[word] = (acc[word] || 0) + 1;
+      }
+      return acc;
+    }, {});
+    setWordCount(wordFrequency);
+  };
+
+  const handleFrequencyCount = () => {
+    const wordFrequency = calculateWordFrequency(text);
+    setWordCount(wordFrequency);
+    setShowWordFrequency(true);
+    props.showAlert(
+      "Check Analysed Frequency in the bottom of the page!",
+      "success"
+    );
+  };
+
+  const handleDownloadClick = () => {
+    const content = `${text}`;
+
+    const blob = new Blob([content], { type: "text/plain;charset=utf-8" });
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = "textutils.txt";
+    link.click();
+
+    URL.revokeObjectURL(link.href);
+    props.showAlert("File downloading!", "success");
+  };
+
   return (
     <>
       <div
@@ -85,10 +148,10 @@ export default function TextForm(props) {
         ></textarea>
 
         <button onClick={handleUpClick} className="btn btn-primary m-1">
-          Convert to Uppercase
+          Convert to Upper Case
         </button>
         <button onClick={handleDownClick} className="btn btn-primary m-1">
-          Convert to LowerCase
+          Convert to Lower Case
         </button>
         <button onClick={handleClearClick} className="btn btn-primary m-1">
           Clear Text
@@ -98,6 +161,12 @@ export default function TextForm(props) {
         </button>
         <button onClick={handleExtraSpaces} className="btn btn-primary m-1">
           Remove Extra Spaces
+        </button>
+        <button onClick={handleFrequencyCount} className="btn btn-primary m-1">
+          Word Frequency
+        </button>
+        <button onClick={handleDownloadClick} className="btn btn-primary m-1">
+          Download Text
         </button>
 
         <div className="container my-3">
@@ -115,8 +184,19 @@ export default function TextForm(props) {
           <p id="box">
             {text.length > 0
               ? text
-              : "Enter something in the textbox above to preview it here"}
+              : "Enter something in the textbox above to preview it here!!!"}
           </p>
+
+          {showWordFrequency && (
+            <>
+              <h3>Word Frequency</h3>
+              <div>
+                {Object.entries(wordCount).map(([word, count]) => (
+                  <div key={word}>{`${word}: ${count}`}</div>
+                ))}
+              </div>
+            </>
+          )}
         </div>
         {/* <div className="ratio ratio-16x9">
             <iframe
